f623ae077666717f4b90b73adb42ab9ad0a25cd3
printablebyhassan-oss <printablebyhassan@gmail.com>
2025-10-22 23:37:52 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Fifa23 Free coins</title>
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    	<link href="https://fonts.googleapis.com/css?family=Roboto:300&display=swap" rel="stylesheet">
+
+    <script src="https://kit.fontawesome.com/04fd5ece3f.js"></script>
+
+		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+    
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+    <link rel="stylesheet" type="text/css" href="css/owl.theme.default.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		
+
+
+		
+		
+	</head>
+	<body style="background:;">
+
+		<div class="netfl-wrap-page col-lg-12 center-block" style="background: ; border:3px solid #114162; overflow: auto;">
+			<div class="page-title-wrap">
+				<div class="leayer-bg">
+				<div class="page-title center-block text-center error-title" >
+					<img src="img/logo.png" style="height: 55px; padding-top: 10px;" class="center-block">
+					<h1 style="color: #000">Fifa23</br> <span style="font-size: 16px;""> Free coins </span></h1>
+					<p style="margin-bottom: 0px;
+    padding-bottom: 20px;"> Get coins to purchase In-game upgrades  </p>
+						
+				</div>
+				</div>
+			</div>
+
+			<div class="page-content text-center" style="background: ;">
+				
+
+				<div class="tab-content">
+					<div role="tabpanel" class="tab-pane fade active in netflapp-wrap" id="netflapptool" style="background: ; ">
+						<div class="netflapp-connect" >
+								<div class="col-lg-12 wrappp text-center" style="overflow: auto; padding-left: 0px; padding-right: 0px; ">	
+									<a href="#">1</a>
+									<p>Click to connect</p>
+								</div>
+								<div class="input">
+									<input type="text" id="username_rev"  placeholder="Username" style="text-align: left;">
+								</div>
+								<div class="loading" style="display: none;">
+									<p class="connect-text">Connecting...</p>
+									<div class="spinner">
+  <div class="double-bounce1"></div>
+  <div class="double-bounce2"></div>
+</div>
+								</div>
+								<div class="col-lg-12 connect-btn-wrap">
+								<a href="#" id="connect">Connect</a>
+								 
+								</div>
+
+								<div class="col-lg-12 connecting " style="display: none;">
+									<p><i class="fas fa-check"></i></p>
+								</div>
+						</div>
+
+
+							
+
+						<div class="want" style="opacity: 0.5">	
+						<div class="col-sm-12 want-tosee center-block text-center" style="padding-right: 0px; padding-left: 0px;">
+								<div class="col-lg-12 wrappp" >	
+									<a href="#">2</a>
+									<h4 style="color: #757575">Choose a Plan</h4>
+									<p>Select the plan (for free)</p>
+								</div>
+							<div class="netfl-list center-block col-lg-12">		
+								<ul>
+									<li class="selected">
+										<a href="#">
+											
+											<span><img src="img/coins.png" class="rob-img" alt=""></span>
+											<span class="price">400</span>
+											<span class="free"><span class="free-text">FREE</span></span>
+
+
+											
+											
+										</a>
+									</li>
+
+
+									<li>
+										<a href="#">
+											
+											<span><img src="img/coins.png" class="rob-img" alt=""></span>
+											<span class="price">800</span>	
+											<span class="free"><span class="free-text">FREE</span></span>
+											
+										</a>
+									</li>
+
+									<li>
+										<a href="#">
+											
+											<span><img src="img/coins.png" class="rob-img" alt=""></span>
+											<span class="price">1000</span>	
+											<span class="free"><span class="free-text">FREE</span></span>
+											
+										</a>
+									</li>
+
+
+									
+								</ul>
+							</div>
+						</div>
+
+						<div class="col-lg-12 netflapp-btn-wrap">
+							<a href="#" class="netflapp">Choose Coins amount</a>
+						</div>
+
+						</div>
+
+						<div class="netflapp-start" style="display: none;">
+							<div class="netfl-progress center-block" style="width: 70%; text-align: center; position: relative;">
+
+								
+
+								<h3 style="margin-bottom: 3px;">Adding Free coins Plan:</br> <span id="netfl-text" style="font-weight: 600;"></span></h3>
+								<p  id="netfl-progress-text">Tool try to netflapp username</p>
+								<div class="spinner">
+								  <div class="double-bounce1"></div>
+								  <div class="double-bounce2"></div>
+								</div>
+						         <div class="description">
+							         <p>Please Wait!</p>
+								 </div>
+							</div>
+
+							
+						</div>
+
+						<div class="verification" style="display: none; padding: 10px;">
+							<div id="one">
+								
+							</div>
+						</div>
+						
+					</div>
+
+					
+					
+				</div>
+			</div>
+		</div>
+
+
+		<div id="netfl-connect-err" class="mfp-hide white-popup-block text-center popup-style">
+			<h4 class="text-center error-popup">Error!</h4>
+			<p class="mfp-close">x</p>
+			<img src="img/logo.png" style="height: 40px;" class="logo-popup center-block">
+			<div class="popup-content text-center">
+				<p>Please enter your Email</p>
+				
+				
+			</div>
+		</div>
+
+		<div id="err" class="mfp-hide white-popup-block text-center popup-style">
+			<h4 class="text-center error-popup">Error!</h4>
+			<p class="mfp-close">x</p>
+			<img src="img/icon-w.png" style="height: 40px;" class="logo-popup center-block">
+			<div class="popup-content text-center">
+				<p>You need to use the Generator in order to post a review</p>
+				
+				
+			</div>
+			
+		</div>
+
+		<div id="ogads_locker" class="mfp-hide white-popup-block text-center popup-style">
+			<h4 class="text-center error-popup">Error!</h4>
+			
+			<img src="img/logo.png" style="height: 40px;" class="logo-popup center-block">
+			<div class="popup-content text-center">
+
+				<h3>This Landing page works only with ogads Content Locker</h3>
+				<p>Enter your locker in js/main.js like in example photo:</p>
+				<a href="img/locker-img.PNG">Download example photo</a>
+				
+				<p>Contact me at skype: <b>live:dreamcas21</b> if you need help</p>
+				
+			</div>
+		</div>	
+
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		
+		
+
+		<script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/owl.carousel.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+ 		
+	</body>
+</html>
